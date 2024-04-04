53bb730bd92e4a98678144854c5f14e9d8ad0881
Sauvikesh <saulal@ucdavis.edu>
2024-04-04 15:12:33 -0700

    feat: reading data from file and sending it back as reply

---
+++ data.js
@@ -0,0 +1,7 @@
+let data = [
+    {id: 0, name:"zero"},
+    {id: 1, name:"one"},
+    {id: 2, name:"two"},
+]
+
+module.exports = data;
--- server.js
+++ server.js
@@ -1,13 +1,29 @@
 // Require the framework and instantiate it
 const fastify = require('fastify')({ logger: true })
+const PORT = 3000;
+
+
+const data = require('./data');
 
 // Declare a route
-fastify.get('/', function handler (request, reply) {
-  reply.send({ hello: 'worldfewfewf' })
+fastify.get('/allData', function handler (request, reply) {
+  reply.send({ data: data })
+})
+
+// Declare a route
+fastify.get('/singleData/:id', function handler (request, reply) {
+  const {id} = request.params;
+
+  if (id > data.length) {
+    reply.send({error: "out of index"})
+  }
+  else {
+    reply.send({ data: data[id] })
+  }
 })
 
 // Run the server!
-fastify.listen({ port: 3000 }, (err) => {
+fastify.listen({ port: PORT }, (err) => {
   if (err) {
     fastify.log.error(err)
     process.exit(1)
