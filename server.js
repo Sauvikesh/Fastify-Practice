f35dfa4483099bf87a8ce17577d43a3a130aff25
Sauvikesh <saulal@ucdavis.edu>
2024-04-04 17:31:55 -0700

    feat: added error check, changed to type module

--- server.js
+++ server.js
@@ -1,25 +1,29 @@
 // Require the framework and instantiate it
-const fastify = require('fastify')({ logger: true })
+import Fastify from 'fastify';
 const PORT = 3000;
 
+const fastify = Fastify({
+  logger: true
+})
 
-const data = require('./data');
+fastify.get('/', function handler (request, reply) {
+  reply.send({ hello: "world" })
+})
 
-// Declare a route
 fastify.get('/allData', function handler (request, reply) {
   reply.send({ data: data })
 })
 
 // Declare a route
 fastify.get('/singleData/:id', function handler (request, reply) {
-  const {id} = request.params;
+const {id} = request.params;
 
-  if (id > data.length) {
-    reply.send({error: "out of index"})
-  }
-  else {
-    reply.send({ data: data[id] })
-  }
+if (id > data.length) {
+  reply.send({error: "out of index"})
+}
+else {
+  reply.send({ data: data[id] })
+}
 })
 
 // Run the server!
